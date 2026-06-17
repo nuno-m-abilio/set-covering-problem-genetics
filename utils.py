997b1cdf2eaa5748d0d1cf0080ee7a0dd7060c88
Enzo Vignotti <152637244+enzo-vignotti@users.noreply.github.com>
2026-06-17 14:50:42 -0300

    leitura-csv e boxplot
    primeira implementação do módulo de leitura do arquivo CSV e da construção de um gráfico boxplot

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
-from typing import List
+from typing import List, Dict
+import csv
+
 
 def ExtraiDadosLinha(linha: str) -> List[float]:
     dadosLinha = []
@@ -37,3 +39,43 @@ def LerDados(caminho:str) -> List[List[float]]:
         dados : List[List[float]] = ExtraiDados(file)
 
     return dados
+
+
+# LEITURA DO ARQUIVO CSV PARA O BOXPLOT -------------------------------------
+
+def _module(x:float) -> float:
+    """
+    retorna o módulo de x
+    """
+    return x if (x >= 0) else -x
+
+
+def _ToDict(linha:List[str]) -> Dict[str, List[float]]:
+    """
+    Recebe uma *linha* de um arquivo CSV e o converte em um dicionário.
+    O primeiro elemento de *linha* é considerado o ID da instância e os demais
+    elementos como números inteiros representando o resultado de execucoes
+    do algoritmo genetico
+    """
+
+    n:int = len(linha)
+    instanceID:str = linha[0]
+    referenceValue : float = float(linha[1])
+    elems:List[float] = []
+
+    for i in range(2, n):
+        result : float = float(linha[i])
+        percentual : float = _module((referenceValue - result)/referenceValue)
+        elems.append(percentual)
+    
+    return {instanceID : elems}
+
+def LerCSV(caminho:str) -> Dict[str, List[float]]:
+    data : Dict[str, List[float]] = {}
+
+    with open(caminho, newline='') as CSVfile:
+        reader = csv.reader(CSVfile)
+        for l in reader:
+            data.update(_ToDict(l))
+    
+    return data
